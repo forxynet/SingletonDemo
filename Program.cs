bd8fbc2631eaa0ff6ff454b52c737f8070fbfbbe
Hakan <forxynet@gmail.com>
2020-08-24 22:51:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SingletonSample {
+    public class Singleton {
+        static Singleton singleton;
+        public Guid Id;
+        public Singleton() {
+            Id = Guid.NewGuid();
+        }
+        public static Singleton GetSingleton() {
+            if (singleton == null) {
+                singleton = new Singleton();
+            }
+            return singleton;
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine($"{i}= {Singleton.GetSingleton().Id.ToString()}");
+            }
+        }
+    }
+}
